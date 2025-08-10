6476f0b0efda5f1a89bdedf53c96d4e04e5ae17c
Yasheeb_Faridi <126257671+yasheeb-faridi@users.noreply.github.com>
2025-08-10 12:31:09 +0530

    Reviewed and Optimized Linear search
    Used .index() built-in Python method and tested and elapsed time for every code is in the screenshot.

---
+++ linear-search/linear_search_time3.py
@@ -0,0 +1,27 @@
+import time
+
+def linear_search(list, target):
+    try:
+        return list.index(target)
+    except ValueError:
+        return None
+
+def verify(index):
+    if index is not None:
+        print(f'Target found at index: {index}')
+    else:
+        print("Target not found in the list.")
+
+
+tar_int = int(input("Enter an integer from 1 to 1000000: "))
+nums = [x for x in range(1, 1000000)]
+
+## Putting the counter here to exclude input taking time
+start_time = time.perf_counter()                                                    
+
+result = linear_search(nums, tar_int)
+verify(result)
+
+end_time = time.perf_counter()
+elapsed_time = end_time - start_time
+print(f"Elapsed time: {elapsed_time:.6f} seconds.")
---
+++ linear-search/linear_search_time4.py
@@ -0,0 +1,13 @@
+import time
+
+nums = list(range(1, 1000000))
+tar_int = int(input("Enter an integer from 1 to 1000000: "))
+
+start_time = time.perf_counter()
+
+result = nums.index(tar_int) if tar_int in nums else None
+print(f"The target {tar_int} found at index: {result}")
+
+end_time = time.perf_counter()
+elapsed_time = end_time - start_time
+print(f"Elapsed time: {elapsed_time:.6f} seconds.")
